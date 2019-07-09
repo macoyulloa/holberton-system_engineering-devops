ce15cc3a264cc626b952cd18106b9fd0cfb10ea7
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-07-09 18:10:08 +0000

    [FILE] uploading code point 100, advanced task

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+from=ARGV[0].scan(/(?<=from:)[^\]]*/).join
+to=ARGV[0].scan(/(?<=to:)[^\]]*/).join
+flag=ARGV[0].scan(/(?<=flags:)[^\]]*/).join
+print from + "," + to + "," + flag + "\n"
