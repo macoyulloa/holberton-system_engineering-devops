955448c0ab31189e28e1f4368e794b349e010ef5
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-21 03:27:04 +0000

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,3 +2,7 @@
 exec { '/usr/bin/env apt-get update': }
 exec { '/usr/bin/env apt-get -y install nginx': }
 exec { '/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
+exec { '/usr/bin/env sed -i "42i\ location /redirect_me { return 301 https://www.google.com; }" /etc/nginx/sites-available/default': }
+exec { '/usr/bin/env sed -i "46i\ error_page 404 /custom_404.html;\nlocation = /custom_404.html {\nroot /var/www/html;\ninternal; }':}
+exec { '/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec { '/usr/bin/env service nginx restart': }
